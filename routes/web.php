62cac18663f11c5ef031b9af33935e0ed430a12b
tebetebe19 <83097428+tebetebe19@users.noreply.github.com>
2025-02-18 08:11:07 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -5,8 +5,8 @@ use App\Http\Controllers\ProductsController;
 use Illuminate\Support\Facades\Route;
 
 Route::group(['prefix' => '/'], function () {
-    Route::get('/gallery', [GalleryController::class, 'gallery']);
-    Route::get('/', [ProductsController::class, 'test']);
-    Route::get('{slug?}', [ProductsController::class, 'test']);
+    //Route::get('/gallery', [GalleryController::class, 'gallery']);
+    //Route::get('/', [ProductsController::class, 'test']);
+    //Route::get('{slug?}', [ProductsController::class, 'test']);
     // Route::get('/test', [ProductsController::class, 'test']);
 });
